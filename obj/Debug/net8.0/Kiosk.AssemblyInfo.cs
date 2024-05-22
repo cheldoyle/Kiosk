f7ed7190f00516eb05be2fc5890e2e4a6c1a3e64
Chel <chel.doyle@mscoding.org>
2024-05-22 09:40:11 -0500

    up

--- obj/Debug/net8.0/Kiosk.AssemblyInfo.cs
+++ obj/Debug/net8.0/Kiosk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kiosk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3bd7e50bc23930e8bcb0072ce54e4494853286b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kiosk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kiosk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
